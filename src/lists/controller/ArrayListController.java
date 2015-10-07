20fb07730ee60f9d932f1fadfa72ef46ab6dc3e1
iceflower99 <mhal4767@740s-pl-125.local>
2015-10-07 09:56:15 -0600

    added methods

--- src/lists/controller/ArrayListController.java
+++ src/lists/controller/ArrayListController.java
@@ -1,11 +1,30 @@
 package lists.controller;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 public class ArrayListController
 {
+	private ArrayList<String> firstWords;
+	
+	public ArrayListController()
+	{
+		firstWords= new ArrayList<String>();
+	}
 	
 	public void start()
 	{
-		
+		firstWords.add("fjewoadsuiefhiuf yaw;re au");
+		firstWords.add("babababab");
+	    showList();
+	}
+	
+	private void showList()
+	{
+		for(int spot=0; spot<firstWords.size(); spot++)
+		{
+			JOptionPane.showMessageDialog(null,firstWords.get(spot)+ " ist at spot# " + spot);
+		}
 	}
 
 }
